18f58bf77d3292ebe3b5ed9ba9fea21f14b2111b
Snorker8406 <snorker84@gmail>
2016-09-10 20:08:19 -0500

    login y farmacias servicios agregados

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var md5 = require('MD5');
 var app = express();
 var morgan = require('morgan');
 var consultorio = require('./services/consultorio.js');
+var farmacia = require('./services/farmacia.js');
+var login = require('./services/login.js');
 var listen_port = process.env.PORT || 3000;
 var dev = process.env.DEV;
 
@@ -47,6 +49,8 @@ REST.prototype.configureExpress = function(pool) {
   app.use(morgan("dev"));
   app.use(express.static("./static"));
   var consultorio_router = new consultorio(router, pool, md5);
+  var login_router = new login(router, pool, md5);
+  var farmacia_router = new farmacia(router, pool, md5);
   self.startServer();
 }
 REST.prototype.startServer = function() {
